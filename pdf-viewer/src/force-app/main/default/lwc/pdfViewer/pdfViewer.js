c5f8251de911872d764a7996641604b1d06edea3
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2021-03-17 06:14:51 -0400

    init

--- pdf-viewer/src/force-app/main/default/lwc/pdfViewer/pdfViewer.js
+++ pdf-viewer/src/force-app/main/default/lwc/pdfViewer/pdfViewer.js
@@ -9,7 +9,7 @@ export default class PdfViewer extends LightningElement {
     }
     get url() {
         // refer doc: https://github.com/mohan-chinnappan-n/lwc-components/tree/master/pdf-viewer
-        return '/sfc/servlet.shepherd/document/download/' + this.fileId;
+         return `/sfc/servlet.shepherd/document/download/${this.fileId}`;
     }
 
 }
